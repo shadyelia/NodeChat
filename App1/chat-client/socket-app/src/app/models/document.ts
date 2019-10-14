1effd70f48275f40a754770f7895d05f7eb0da56
shadyelia <shady.elia@orca-tech.net>
2019-10-14 16:50:56 +0200

    removing unused apps and init the final one

--- App1/chat-client/socket-app/src/app/models/document.ts
+++
@@ -1,4 +0,0 @@
-export class Document {
-  id: string;
-  doc: string;
-}
--- Client/chat/src/app/app.module.ts
+++ Client/chat/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from "./app.component";
 import { SocketIoModule, SocketIoConfig } from "ngx-socket-io";
 
 const config: SocketIoConfig = { url: "http://localhost:3000", options: {} };
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [FormsModule, BrowserModule, SocketIoModule.forRoot(config)],
+  imports: [BrowserModule, FormsModule, SocketIoModule.forRoot(config)],
   providers: [],
   bootstrap: [AppComponent]
 })
