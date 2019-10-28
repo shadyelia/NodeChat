7a3dbfc9ecef71b0dd20ac41221568c56d2adb21
ShadyElia <shady.elia@hotmail.com>
2019-10-28 22:56:16 +0200

    finish v1

--- Client/chat/src/environments/environment.prod.ts
+++ Client/chat/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   azureStorage: {
-    containerName: "samplecontaier",
+    containerName: "",
     storageAccessToken:
-      "?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2019-10-27T18:00:30Z&st=2019-10-27T10:00:30Z&spr=https&sig=HnS9SGu592e7%2F0sPkpwJhcY3MClD2lankS68YFqhfW4%3D",
-    storageUri: "https://orcatechsotrage.blob.core.windows.net"
+      "",
+    storageUri: ""
   }
 };
--- Client/chat/src/environments/environment.ts
+++ Client/chat/src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
   production: false,
   azureStorage: {
-    containerName: "samplecontaier",
+    containerName: "",
     storageAccessToken:
-      "?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2019-10-27T18:00:30Z&st=2019-10-27T10:00:30Z&spr=https&sig=HnS9SGu592e7%2F0sPkpwJhcY3MClD2lankS68YFqhfW4%3D",
-    storageUri: "https://orcatechsotrage.blob.core.windows.net"
+      "",
+    storageUri: ""
   }
 };
 
